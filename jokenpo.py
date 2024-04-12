90700fa021ee71fa5c08e203287906360675a85d
Leticia <166722628+Andrade-Leticia@users.noreply.github.com>
2024-04-12 11:11:17 -0300

    Update jokenpo.py
    atualização com o modo de jogo "Humano vs Computador" :)

--- jokenpo.py
+++ jokenpo.py
@@ -12,4 +12,29 @@ while escolha == '1':
 
         if jogada1 == "pedra" and jogada2 == "papel":
                 print("jogador 1 ganhou! ")
-                
+
+############################### MODO HUMANOxCOMPUTADOR ##################################################
+import random
+
+print("Bem vindo(a) ao jogo digital de Jokenpô!")
+escolha = print("Nesse jogo, você pode jogar de três modos: HUMANOxHUMANO, HUMANOxCOMPUTADOR, COMPUTADORxCOMPUTADOR")
+escolha1 = print("1. HUMANOxHUMANO")
+escolha2 = print("2. HUMANOxCOMPUTADOR")
+escolha3 = print("3. COMPUTADORxCOMPUTADOR")
+print(input("Escolha seu modo:"))
+
+###### MODO 2 (HUMANOxCOMPUTADOR)
+while escolha == escolha2:
+        print("1. Pedra")
+        print("2. Papel")
+        print("3. Tesoura")
+        jogador = int(input("Digite o número de sua jogada: "))
+        computador = random.randint(1,3)
+        print(computador)
+        if jogador == computador:
+                print("Empate!")
+        elif (jogador == 1 and computador == 3) or (jogador == 2 and computador == 1) or (jogador == 3 and computador ==2):
+                print("Você venceu!!")
+        else:
+                print("Você perdeu!")
+
